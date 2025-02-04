b80edb622571f84ff98df44a08d2814786259d55
prachik167 <128143076+prachik167@users.noreply.github.com>
2025-02-04 12:21:04 +0530

    Add files via upload

---
+++ Interface/IEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal interface IEmployee
+    {
+        void Display();
+        decimal calCulateSalary();
+    }
+    class Salary : IEmployee {
+        public string name { get; set; }
+        public int id { get; set; }
+        public decimal baseSalary { get; set; }
+        public decimal bonus { get; set; }
+        public void Display()
+        {
+            Console.WriteLine($"Employee Name : {name} Employee Id : {id} Base Salary : {baseSalary} bonus : {bonus}");
+        }
+        public decimal calculateSalary()
+        {
+            return baseSalary + bonus;
+        }
+    }
+}
