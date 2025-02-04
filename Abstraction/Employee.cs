d4c8633b6d040bef7a3331815f534ae55b7c9675
prachik167 <128143076+prachik167@users.noreply.github.com>
2025-02-04 12:13:24 +0530

    Add files via upload

---
+++ Abstraction/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    abstract class Employee
+    {
+        public int empId { get; set; }
+        public string empName { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine($"Employee Id : {empId} Employee Name: {empName} ");
+        }
+        public abstract decimal calculateSalary();
+    }
+
+    class salary : Employee
+    {
+        public decimal baseSalary { get; set; }
+        public decimal bonus { get; set; }
+
+        public override decimal calculateSalary()
+        {
+            return baseSalary + bonus; ;
+        }
+    }
+}
---
+++ Abstraction/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Abstraction
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            salary s = new salary() { empId = 1, empName = "Prachi", baseSalary = 50000, bonus = 5000 };
+            s.Display();
+            Console.WriteLine($"Total Salary : {s.calculateSalary()}");
+        }
+    }
+}
